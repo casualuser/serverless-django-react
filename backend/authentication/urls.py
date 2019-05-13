d4ab52d3a7c665c2073ace296388a2432d35ddb0
Steven Phaedonos <steven@phaedonos.com>
2019-05-13 23:45:36 +1000

    Add url for MSAL authentication error endpoint

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -5,4 +5,5 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     url("login/", views.login),
     url("refresh/", TokenRefreshView.as_view()),
+    url("error/", views.error),
 ]
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework import views, status
+from django.http import HttpResponse
 import requests
 import os
 import logging
