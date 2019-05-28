f48d7c401ef1536f90ff31a27e922d0d6d45deb7
Steven Phaedonos <steven@phaedonos.com>
2019-05-28 15:36:24 +1000

    Custom error responses for CloudFront distros

--- pipeline/cloudfront.tf
+++ pipeline/cloudfront.tf
@@ -26,6 +26,19 @@ resource "aws_cloudfront_distribution" "stage_distribution" {
     ssl_support_method = "sni-only"
   }
 
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code = 404
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
   default_cache_behavior {
     allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods = ["GET", "HEAD"]
@@ -76,6 +89,20 @@ resource "aws_cloudfront_distribution" "prod_distribution" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code = 404
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
   default_cache_behavior {
     allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods = ["GET", "HEAD"]
